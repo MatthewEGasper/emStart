bfe13213e3c672ac14c4d1665236353d12005cb6
Matthew Grabasch <matthew.grabasch@gmail.com>
2021-10-24 23:23:18 -0400

    Library Instructions

--- Software/groundStation/emStart_rotator_code/emStart_rotator_code.ino
+++ Software/groundStation/emStart_rotator_code/emStart_rotator_code.ino
@@ -21,6 +21,7 @@
 // Revision 0.0 - File Created
 //
 // Additional Comments:
+// Must install Servo library to run, see readme
 //
 ////////////////////////////////////////////////////
 #include <FeedBackServo.h>
@@ -31,11 +32,17 @@
 #define AZIMUTH_SERVO_PIN 3
 #define ANGLE_SERVO_PIN 4
 
+//define an offset from True North of the Azimuth servo
+#define OFFSET_FROM_NORTH 0
+
 // set feedback signal pin number
 FeedBackServo azimuth_servo = FeedBackServo(FEEDBACK_PIN);
 
 //set angle servo control pin
 Servo angle_servo; 
+
+int azimuth_set = 0;
+int angle_set = 0;
 
 void setup() {
     // set servo control pin number
@@ -52,16 +59,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+<<<<<<< Updated upstream
   //set_coord();
   Serial.println("Test");
   delay(1000);
+=======
+  azimuth_set = Serial.read();
+  if (((azimuth_set >= 0)||(azimuth_set <= 360))&&((angle_set >= 0)||(angle_set <= 90)))
+  {
+    set_coord(azimuth_set,angle_set);
+  }
+>>>>>>> Stashed changes
 }
 
 void set_coord(float azimuth, float angle)
 {
   //assumed that North is 0 Degrees
-  azimuth_servo.rotate(azimuth,1);
-
+  while((azimuth_servo.Angle() - OFFSET_FROM_NORTH) != azimuth)
+  {
+    azimuth_servo.rotate((azimuth - OFFSET_FROM_NORTH),1);
+  }
   angle_servo.write(angle);
 
 }
